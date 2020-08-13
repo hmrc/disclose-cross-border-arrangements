4ce076ff474ef4589a4224a85e01fc0227895632
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-08-13 18:05:07 +0100

    DAC6-106: Create endpoint for count of user submissions

--- it/repositories/SubmissionDetailsRepositorySpec.scala
+++ it/repositories/SubmissionDetailsRepositorySpec.scala
@@ -61,6 +61,35 @@ class SubmissionDetailsRepositorySpec
       }
     }
 
+    "must correctly count submission details" in {
+      val app: Application = new GuiceApplicationBuilder().build()
+      val repo: SubmissionDetailsRepository = app.injector.instanceOf[SubmissionDetailsRepository]
+
+      running(app) {
+
+        database.flatMap(_.drop()).futureValue
+        await(repo.storeSubmissionDetails(submissionDetails))
+
+        whenReady(repo.countNoOfPreviousSubmissions("enrolmentID")) {
+          _ mustEqual 1
+        }
+      }
+    }
+
+    "must correctly return a zero count when user has not submitted" in {
+      val app: Application = new GuiceApplicationBuilder().build()
+      val repo: SubmissionDetailsRepository = app.injector.instanceOf[SubmissionDetailsRepository]
+
+      running(app) {
+
+        database.flatMap(_.drop()).futureValue
+
+        whenReady(repo.countNoOfPreviousSubmissions("enrolmentID")) {
+          _ mustEqual 0
+        }
+      }
+    }
+
     "must store submission details" in {
       val app: Application = new GuiceApplicationBuilder().build()
       val repo: SubmissionDetailsRepository = app.injector.instanceOf[SubmissionDetailsRepository]
