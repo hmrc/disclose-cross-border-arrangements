37fbecde68e3993baec2b5826c0e24fff1b49666
scheung38 <1273014+scheung38@users.noreply.github.com>
2022-07-28 15:06:54 +0100

    hmrc-mongo-play-28 -> 0.68.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-27"              % "0.50.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"              % "0.68.0",
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"       % "5.24.0",
     "com.typesafe.play"       %% "play-iteratees"                  % "2.6.1",
     "org.typelevel"           %% "cats-core"                       % "2.1.1"
